b39e2f2cca3d5b811dee71a769761c0a747db23c
Nick Valdez <nicholastvaldez@gmail.com>
2023-01-23 15:52:41 -0600

    add pythondunder init packages for models and views

---
+++ models/snake.py
@@ -0,0 +1,12 @@
+class Snake():
+
+    # Class initializer. It has 5 custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, id, name, owner_id, species_id, gender, color):
+        self.id = id
+        self.name = name
+        self.owner_id = owner_id
+        self.species_id = species_id
+        self.gender = gender
+        self.color = color
